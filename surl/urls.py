73247a83229b5cfd12d63585b92e05118e70dfc1
David Mugnai <dvd@develer.com>
2010-01-26 14:39:01 +0100

    new view, used by a jsonp call to short an url

--- surl/urls.py
+++ surl/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+    url(r'^short_url$', 'surl.views.jsonp_short_url', name='surl-jsonp-short-url'),
     url(r'^(?P<url>.*)$', 'surl.views.redirect', name = 'surl-redirect'),
 )
 
--- surl/views.py
+++ surl/views.py
@@ -2,6 +2,7 @@
 import models
 import settings
 
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
@@ -13,3 +14,26 @@ def redirect(request, url):
         'ga': settings.GOOGLE_ANALYTICS,
     }
     return render_to_response('surl/redirect.html', ctx, context_instance = RequestContext(request))
+
+def jsonp_short_url(request):
+    try:
+        url = request.GET['q']
+    except KeyError:
+        return HttpResponseBadRequest(content='invalid request')
+    return HttpResponse(content='document.log("%s");' % make_short_url(url))
+
+def make_short_url(url, code=None, reuse=True):
+    short = None
+    if reuse:
+        try:
+            short = models.ShortUrl.objects.filter(url=url)[0]
+        except IndexError:
+            pass
+    if short is None:
+        title = ''
+        if code:
+            short = models.ShortUrl.new_url(url, title, id=code)
+        else:
+            short = models.ShortUrl.new_url(url, title)
+    return settings.URL_PREFIX + short.id
+

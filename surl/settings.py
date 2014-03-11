5cc5cb31f7a8284a6ccc117c7af72471a02c67fd
Andrea Stagi <stagi.andrea@gmail.com>
2014-03-11 18:06:51 +0100

    Restore Analytics and settings

--- surl/settings.py
+++ surl/settings.py
@@ -26,10 +26,10 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-"""try:
+try:
     GOOGLE_ANALYTICS = settings.SURL_GOOGLE_ANALYTICS
 except AttributeError:
-    raise ImproperlyConfigured('google analytics code not found')"""
+    raise ImproperlyConfigured('google analytics code not found')
 
 ABSOLUTE_PREFIX_REDIRECT_SERVICE = getattr(settings, 'SURL_ABSOLUTE_PREFIX_REDIRECT_SERVICE', None)
 ABSOLUTE_PREFIX_JSONP_SERVICE = getattr(settings, 'SURL_ABSOLUTE_PREFIX_JSONP_SERVICE', None)
